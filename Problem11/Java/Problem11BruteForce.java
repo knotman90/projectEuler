8a980f0621182b3b02932ea55417f5edc6abc678
knotman90 <davidespataro@davidespataro.it>
2015-03-15 14:01:20 +0100

    added problem11 solution in java improvement

--- Problem11/Java/Problem11BruteForce.java
+++ Problem11/Java/Problem11BruteForce.java
@@ -5,10 +5,12 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+
+
 public class Problem11BruteForce {
 	public static int size=4;
+	public static long maxPossibleValue=multiplicateKtimes(99,size);
 
-	
 	public static long getProductElmts(int[][] matrix, int i, int j, int inci, int incj){
 		/**
 		 * return the product of the tuple of *size element* starting at i,j in the direction of the vector (inci,incj)
@@ -21,6 +23,18 @@ public class Problem11BruteForce {
 		return product;
 	}
 
+	private static long multiplicateKtimes(int num, int size2) {
+		long res=1;
+		for(int i=0;i<size2;i++)
+			res*=num;	
+
+		return res;
+	}
+
+	/*
+	 * There is no need to check all the 8 directions-> 4 are enough
+	 * Return as soon I get (if happen) the max possible value = 99^size
+	 * */
 	public static long findMaxProduct(int[][] matrix){
 		int m = matrix.length;
 		int n = matrix[0].length;
@@ -39,14 +53,14 @@ public class Problem11BruteForce {
 				//diagonal  right
 				if(j<n-size-1 && i<n-size-1)
 					diagR=getProductElmts(matrix, i, j, 1, 1);
-				
+
 				//diagonal  left
 				if(j>size-1 && i<n-size-1)
 					diagLeft=getProductElmts(matrix, i, j, 1, -1);
 
 				max=Math.max(max,Math.max(diagLeft,Math.max(diagR,Math.max(prodHor, vertDown))));
-				if(max==70600674){
-					System.out.println("i,j "+i+", "+j);
+				
+				if(max>=maxPossibleValue){
 					return max;
 				}
 
