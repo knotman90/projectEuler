3eeb1a233440c34c5a08929d7d872efcb96e75dd
Davide Spataro <d.spataro@mat.unical.it>
2015-03-21 16:10:00 +0100

    commented code/strategy

--- Problem_67_18/Problem6718.cpp
+++ Problem_67_18/Problem6718.cpp
@@ -11,7 +11,28 @@
 
 using namespace std;
 
+/**
+ * Strategy:
+ * Read the matrix from the file
+ * Iteratively starting from level zero compute the longest path to each node
+ * by checking the alternative route (at most two for each node).
+ *
+ *INPUT:
+ * 	75
+	95 64
+	17 47 82
+	18 35 87 10
+	20 4 82 47 65
 
+	OUTPUT (max of the last row):
+	75
+	170 139
+	187 217 221
+	205 252 308 231
+	225 256 390 355 296
+
+ *
+ * */
 void solveProblem(unsigned long** n, int X){
 	for(int i=1;i<X;i++){
 		for(int j=0;j<=i;j++){
@@ -42,15 +63,16 @@ int main(){
 	Y=X;
 	unsigned int level=1000;
 	unsigned long** n;
-	unsigned long** nC;
+
 
 	n=allocateMatrix(X,Y);
 	initializeMatrix(n,X,Y);
 	readPiramydMatrixFromFile(path,n,X,Y);
-	//printMatrix_if(n,X,Y,[](unsigned long a)->bool {return a!=0;});
+	printMatrix_if(n,X,Y,[](unsigned long a)->bool {return a!=0;});
 	solveProblem(n,X);
 	unsigned long max = arrayMax(n[X-1],X);
 	cout<<"The longest path cost is: "<<max<<endl;
+	printMatrix_if(n,X,Y,[](unsigned long a)->bool {return a!=0;});
 	deallocateMatrix(n,X);
 }
 
