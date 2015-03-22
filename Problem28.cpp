d019bdc75238e7ddad1a61b892d4ac009d0b5a0a
knotman90 <davidespataro@davidespataro.it>
2015-03-22 20:03:43 +0100

    Problem28

--- Problem28.cpp
+++ Problem28.cpp
@@ -3,11 +3,19 @@
 #include "matrixUtils.h"
 
 using namespace std;
+void initMatrix(unsigned long **m , int X, int Y){
+int c=0;
+int tr=1;
+for(int c=0;c<X/2;c++){
+	tr=c*4+1;
+cout<<tr<<endl;
+}
 
+}
 
 int main(){
 int X,Y;
-
+string path="problem29Input.dat";
 cout<<"Insert Matrix (sqare) size:"<<endl;
 cin>>X; Y=X;
 
